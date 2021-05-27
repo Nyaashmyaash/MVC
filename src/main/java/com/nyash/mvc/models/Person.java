ff02b698c9d9a0f74fb806887c454e5712d74810
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-05-27 08:16:05 +0700

    add changes

--- src/main/java/com/nyash/mvc/models/Person.java
+++ src/main/java/com/nyash/mvc/models/Person.java
@@ -1,9 +1,22 @@
 package com.nyash.mvc.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "name should be between 2 and 30 characters")
     private String name;
+
+    @Min(value = 0, message = "age should be above 0")
     private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     private String email;
 
     public Person(){
